46ad98558aed3d6c4c99d0c1d7376cb2abaee96a
jonathanortegamol <jhojaormo@gmail.com>
2025-07-15 23:20:55 -0500

    ADD Entity User y News

--- distributednewsbackend/src/main/java/com/example/distributednewsbackend/model/News.java
+++ distributednewsbackend/src/main/java/com/example/distributednewsbackend/model/News.java
@@ -1,5 +1,64 @@
 package com.example.distributednewsbackend.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="news")
 public class News {
-  
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+  private Long id;
+
+  private String title;
+  private String desc;
+  private String topic;
+
+  public News(){
+
+  }
+
+  public News(String title, String desc, String topic){
+    this.title = title;
+    this.desc = desc;
+    this.topic = topic;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getDesc() {
+    return desc;
+  }
+
+  public void setDesc(String desc) {
+    this.desc = desc;
+  }
+
+  public String getTopic() {
+    return topic;
+  }
+
+  public void setTopic(String topic) {
+    this.topic = topic;
+  }   
+
+
 }
--- distributednewsbackend/src/main/java/com/example/distributednewsbackend/model/User.java
+++ distributednewsbackend/src/main/java/com/example/distributednewsbackend/model/User.java
@@ -1,6 +1,75 @@
 package com.example.distributednewsbackend.model;
 
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name="users")
 public class User {
-  
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+  private String username;
+  private String password;
+
+  @ElementCollection
+  private java.util.List<String> subs;
+
+  public User(){
+
+  }
+
+  public User(String name, String username, String password, java.util.List<String> subs) {
+      this.name = name;
+      this.username = username;
+      this.password = password;
+      this.subs = subs;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public java.util.List<String> getSubs() {
+    return subs;
+  }
+
+  public void setSubs(java.util.List<String> subs) {
+    this.subs = subs;
+  }
+
 }
