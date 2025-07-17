0f9317b718c529cbae87cb2b5ffd0b557e8b4f7a
oscar <oscar.orellana>
2025-07-16 22:37:24 -0500

    Removed repeated news model, added parameters

--- distributednewsbackend/src/main/java/com/example/distributednewsbackend/model/News.java
+++ distributednewsbackend/src/main/java/com/example/distributednewsbackend/model/News.java
@@ -1,10 +1,13 @@
 package com.example.distributednewsbackend.model;
 
+import java.util.Date;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,6 +15,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Table(name="news")
+@AllArgsConstructor
 public class News {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,7 +24,10 @@ public class News {
 
   private String title;
   private String desc;
+  private String lead; //Lead es como el resumen o gancho de la noticia
+  private String author;
   private String topic;
+  private Date publishedDate;
 
   public News(){
 
