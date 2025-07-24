fa983fa2f42fa4ac04e4dd691052898d1087c4fe
jonathanortegamol <jhojaormo@gmail.com>
2025-07-24 15:07:34 -0500

    Fix controller topic RequestBody

--- distributednewsbackend/src/main/java/com/example/distributednewsbackend/controller/TopicController.java
+++ distributednewsbackend/src/main/java/com/example/distributednewsbackend/controller/TopicController.java
@@ -2,13 +2,14 @@ package com.example.distributednewsbackend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.distributednewsbackend.model.Topic;
 import com.example.distributednewsbackend.repository.TopicRepository;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 
 @RestController
 @RequestMapping("/api")
