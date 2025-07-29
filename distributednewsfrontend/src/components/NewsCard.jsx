05e3a4bec802d79bf2ab50e21c3d0275262baacc
jonathanortegamol <jhojaormo@gmail.com>
2025-07-28 20:39:59 -0500

    fix description news

--- distributednewsfrontend/src/components/NewsCard.jsx
+++ distributednewsfrontend/src/components/NewsCard.jsx
@@ -2,7 +2,7 @@ const NewsCard = ( {noticia} ) => {
 	return (
 		<div className="w-full py-4 shadow-md p-5 mb-5 rounded-sm">
 			<h3 className="font-bold">{noticia.title}</h3>
-			<p className="font-light text-sm text-neutral-700">{noticia.desc}</p>
+			<p className="font-light text-sm text-neutral-700">{noticia.description}</p>
 		</div>
 	)
 }
